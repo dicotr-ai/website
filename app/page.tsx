d4a06975e1cc858ea1d40ad1dc12522a7cfdca7b
Nikhil Yadav <yadav9.nikhil@gmail.com>
2025-08-15 22:11:25 +0530

    font configure

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
 const url = process.env.NEXT_PUBLIC_ENVIROMENT
 console.log("hello", url)
   return (
-    <>
+    <div className="font-rubik">
       Hello World {url}
-    </>
+    </div>
   );
 }
