e5137c30f211afe4e7f70b48d0cd9cc412e94b1c
HarshRaj29004 <142659829+HarshRaj29004@users.noreply.github.com>
2025-09-16 22:14:43 +0530

    fewer changes in acknowlegement page

--- app/components/pages/acknowlegde.tsx
+++ app/components/pages/acknowlegde.tsx
@@ -1,9 +1,9 @@
 const Acknowlegde = () => {
     return(
         <>
-            <section className="container mx-auto px-6 py-16 grid md:grid-cols-2 gap-16 items-center">
-                 <div>
-                    <img src="./Acknowledge-section.svg" alt="Team working at a desk" className="w-full h-auto" />
+            <section className="container mx-auto px-0 py-4 grid md:grid-cols-2 gap-20 items-center">
+                 <div className="">
+                    <img src="./Acknowledge-section.svg" alt="Team working at a desk" className="w-full h-[300px]" />
                 </div>
                 <div>
                     <h2   style={{
@@ -22,8 +22,8 @@ const Acknowlegde = () => {
                             lineHeight: '40px',
                             wordWrap: 'break-word',
                         }}>
-                        WeThink is a strategic digital marketing agency focused on helping brands tell their story. From our content strategy to our paid social media to web expertise, we craft end-to-end digital experiences that drive results.
-                        With a team of experienced marketers, copywriters, and designers, we are dedicated to our clients&apos; success.
+                        WeThink is a strategic digital marketing agency focused on helping brands grow with purpose. From content strategy to SEO, and <br />social media to web experience, we craft end-to-end digital <br /> journeys that drive real results.<br />
+                        With a team of experienced marketers, copywriters, and designers, <br />we don&apos;t just deliver services—we build relationships that last.
                     </p>
                 </div>
             </section>
