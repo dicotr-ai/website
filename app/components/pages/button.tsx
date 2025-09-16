87b5c443902b67e5868df7eaaeca00de76609528
HarshRaj29004 <142659829+HarshRaj29004@users.noreply.github.com>
2025-09-16 12:50:45 +0530

    fewer changes in hero sec

--- app/components/pages/button.tsx
+++ app/components/pages/button.tsx
@@ -8,7 +8,7 @@ const Button = ({label,onClick}:ButtonProps) => {
     <div className="relative mt-12 w-[293px] h-[78px]">
       <button
         className="relative w-full h-full rounded-full 
-                   bg-gradient-to-tr from-[#98AA28] to-[#D6FF7E] 
+                   bg-gradient-to-tl from-[#98AA28] to-[#D6FF7E]
                    shadow-lg border border-white 
                    text-white text-lg font-rubik font-semibold tracking-wider
                    cursor-pointer transition-all duration-300
--- app/components/pages/hero.tsx
+++ app/components/pages/hero.tsx
@@ -9,38 +9,44 @@ const rubik = Rubik({
 
 const Hero = () => {
   return (
-    <main className="container mx-auto grid min-h-[80vh] items-center gap-8 px-0 py-26 md:grid-cols-2">
-      {/* Text Content */}
-      <div className="px-10">
-        <div className="text-left">
-          <h1 className={`${rubik.className} text-5xl font-black text-gray-800 md:text-6xl`}>
-            We Think Different. We <br /> Think Digital.
-          </h1>
-          <p className="mt-4 text-lg text-gray-600">
-            At WeThink, we combine strategy, creativity, and analytics <br /> 
-            to help businesses thrive in the digital world.
-          </p>
-            <Button label='Work with Us'/>
-        </div>
-      </div>
+    <section className="relative min-h-[80vh] w-full overflow-hidden bg-white">
+      {/* <div className="container mx-auto grid min-h-[80vh] items-center gap-8 px-0 py-0 md:grid-cols-2 pt-54"> */}
+      <div className='container mx-auto relative'>
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center pt-36">
+          {/* Text Content */}
+          <div className="relative z-20 md:pr-10 px-24">
+            {/* <div className="text-left"> */}
+              <h1 className={`${rubik.className} text-5xl font-black text-gray-800 md:text-6xl`}>
+                We Think Different. We <br /> Think Digital.
+              </h1>
+              <p className="mt-4 text-lg text-gray-600">
+                At WeThink, we combine strategy, creativity, and analytics <br /> 
+                to help businesses thrive in the digital world.
+              </p>
+              <div className="mt-6">
+                <Button label='Work with Us'/>
+              </div>
+          </div>
 
-      {/* Image Section */}
-      <div className="relative h-[500px] w-full">
-        <div 
-          className="absolute right-0 top-0 h-full w-2/3 bg-orange-300" 
-          style={{ clipPath: "polygon(0 100%, 100% 100%, 100% 0)" }} 
-        />
-        <div className="relative w-full h-full">
-          <Image 
-            src="/hero-image.svg" 
-            alt="Hero" 
-            fill 
-            className="object-cover" 
-            draggable={false} 
-          />
+          {/* Image Section */}
+          <div className="relative h-[500px] w-full px-0 -mt-10 md:mt-0 z-10">
+            <div 
+              className="absolute right-0 top-0 h-full w-2/3 bg-orange-300" 
+              style={{ clipPath: "polygon(0 100%, 100% 100%, 100% 0)" }} 
+            />
+            <div className="relative w-full h-full">
+              <Image 
+                src="/hero-image.svg" 
+                alt="Hero" 
+                fill 
+                className="object-cover" 
+                draggable={false} 
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </main>
+    </section>
   );
 };
 
